a777a563a7aff2752e55ac87d3efccdc3e216c7d
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-21 17:34:16 -0600

    a whole bunch. Revamped People.jsx and Show.jsx for more effective code.

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,126 +1,143 @@
-import { useState, useEffect } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
-const Show = (props) => {
-  const [person, setPerson] = useState(null);
-  const [loading, setLoading] = useState(true);
+function Show(props) {
+    const [person, setPerson] = useState(null)
+    const [editForm, setEditForm] = useState("")
+    
+		const navigate = useNavigate()
 
-  const placeholderImage =
-    "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const URL = `http://localhost:4000/people/${id}`;
-  const [newForm, setForm] = useState({
-    name: "",
-    image: "",
-    title: "",
-  });
+    const params = useParams()
+    const { id } = params
 
-  const handleChange = (e) =>
-    setForm({ ...newForm, [e.target.name]: e.target.value });
+    const URL = `http://localhost:4000/people/${id}`
 
-  const updatePerson = async (event) => {
-    event.preventDefault();
-    try {
-      const requestOptions = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newForm),
-      };
-      await fetch(URL, requestOptions);
-      getPerson();
-      setForm({
-        name: "",
-        image: "",
-        title: "",
-      });
-    } catch (err) {
-      console.error(err);
+    // console.log("id", id, URL)
+    // console.log(`Current Person: ${JSON.stringify(person)}`)
+
+    const handleChange = (e) => setEditForm({ ...editForm, [e.target.name]: e.target.value })
+
+
+    const updatePerson = async (e) => {
+        e.preventDefault()
+        // console.log(editForm)
+        try {
+            const options = {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(editForm)
+            }
+            const response = await fetch(URL, options)
+            const updatedPerson = await response.json()
+
+            setPerson(updatedPerson)
+            setEditForm(updatedPerson)
+
+        } catch (err) {
+            console.log(err)
+            navigate(URL)
+        }
     }
-  };
 
-  const getPerson = async () => {
-    try {
-      const response = await fetch(URL);
-      const result = await response.json();
-      console.log(`Get person:`, result);
-      setPerson(result);
-      setLoading(false);
-    } catch (err) {
-      console.log(err);
+    const getPerson = async () => {
+        try {
+
+            const response = await fetch(URL)
+            const foundPerson = await response.json()
+
+            setPerson(foundPerson)
+            setEditForm(foundPerson)
+
+        } catch (err) {
+            console.log(err)
+        }
     }
-  };
 
-  const removePerson = async (e) => {
-    try {
-      const options = {
-        method: "DELETE",
-      };
-      const response = await fetch(URL, options);
-      const deletedPerson = await response.json();
+    const removePerson = async () => {
+        try {
+            const options = {
+                method: "DELETE"
+            }
+            const response = await fetch(URL, options)
+            const deletedPerson = await response.json()
+            // console.log(deletedPerson)
+            navigate('/')
 
-      navigate("/");
-    } catch (err) {
-      console.log(err);
+            // navigate will change the browser's URL
+            // which will cause react-router to "redirect" to home page;
+            // the Main will then re-render the People component
+            // upon mount People will fetch the updated index of people data
+
+        } catch (err) {
+            console.log(err)
+            navigate(URL)
+        }
     }
-  };
 
-  const isLoading = () => <h2>...Loading</h2>;
-  const loaded = () => (
-    <>
-      <div className="person-card">
-        <h1>{person.name}</h1>
-        <div>
-          <p>Delete Person</p>
-          <button onClick={removePerson}> X </button>
-        </div>
-        <img src={person.image || placeholderImage} />
-        <h3>{person.title || "No title given"}</h3>
-      </div>
-      <Link to="/">Back to Home</Link>
-      <section>
-        <h2>Edit this Person</h2>
-        <form onSubmit={updatePerson}>
-          <input
-            key ={newForm._id}
-            type="text"
-            value={newForm.name}
-            name="name"
-            placeholder="name"
-            onChange={handleChange}
-          />
-          <input
-            key ={newForm._id}
-            type="text"
-            value={newForm.image}
-            name="image"
-            placeholder="image URL"
-            onChange={handleChange}
-          />
-          <input
-            key ={newForm._id}
-            type="text"
-            value={newForm.title}
-            name="title"
-            placeholder="title"
-            onChange={handleChange}
-          />
-          <input type="submit" value="Update Person" />
-        </form>
-      </section>
-    </>
-  );
-  useEffect(() => {
-    getPerson();
-  }, []);
+    useEffect(() => {
+        getPerson()
+    }, [])
 
-  return (
-    <section className="ShowContainer">
-      {loading ? isLoading() : loaded()}
-    </section>
-  );
-};
+    const loaded = () => (
+        <>
+            <section>
+                <div className="person">
+                    <h1>Show Page</h1>
+                    <h2>{person.name}</h2>
+                    <h2>{person.title}</h2>
+                    <img src={person.image} alt={person.name + " image"} />
+                    <div>
+                        <button 
+														className="delete" 
+														onClick={removePerson}>
+															Remove Person
+												</button>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2>Edit this Person</h2>
+                <form onSubmit={updatePerson}>
+                    <input
+                        type="text"
+                        value={editForm.name}
+                        name="name"
+                        placeholder="name"
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        value={editForm.image}
+                        name="image"
+                        placeholder="image URL"
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        value={editForm.title}
+                        name="title"
+                        placeholder="title"
+                        onChange={handleChange}
+                    />
+                    <input type="submit" value="Update Person" />
+                </form> 
+            </section>
+        </>
 
-export default Show;
+
+    )
+
+
+    const loading = () => (
+        <>
+            <h1>
+                Loading...
+            </h1>
+        </>
+    );
+    return (
+        <div>{person ? loaded() : loading()}</div>
+    )
+}
+
+export default Show
